519daacb3f25fd7e88d6c9bef21010e32b7ef39a
Ivan Cao-Berg <icaoberg@andrew.cmu.edu>
2022-02-14 23:43:06 -0500

    Added version 0.32.2

---
+++ 0.32.2/rbuild.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Copyright © 2021 Pittsburgh Supercomputing Center.
+# All Rights Reserved.
+
+IMAGE=singularity-lazygit-0.32.2.sif
+DEFINITION=Singularity
+
+if [ -f $IMAGE ]; then
+	rm -fv $IMAGE
+fi
+
+singularity build --remote $IMAGE $DEFINITION
+
+if [ -f $IMAGE ]; then
+	exit 0
+else
+	exit 1
+fi
